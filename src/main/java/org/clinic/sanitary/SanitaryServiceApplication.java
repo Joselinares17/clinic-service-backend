9c20f4d02c938d769ed07e97749ac83ba7addaa8
joselinares17 <josedaniellinares135@gmail.com>
2025-01-23 19:36:14 -0500

    config: updating conf files

--- src/main/java/org/clinic/sanitary/SanitaryServiceApplication.java
+++ src/main/java/org/clinic/sanitary/SanitaryServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SanitaryServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SanitaryServiceApplication.class, args);
 	}
